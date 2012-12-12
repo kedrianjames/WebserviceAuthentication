552b6ef2d2f1ddcfc635993295ab0e12c5c8e7f2
kedrianjames <keg1082@yahoo.com>
2012-12-12 22:13:08 +0200

    initial commit

---
+++ WebServiceAuthentication/AuthenticationService.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Security;
+
+namespace WebServiceAuthentication
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://grocerystore.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]   
+    public class AuthenticationService : System.Web.Services.WebService
+    {
+
+        [WebMethod] //validates a user       
+        public Boolean isValidCredentials(string Username, string Password)
+        {
+            //accesses membership and checks for credential validility
+            if (Membership.ValidateUser(Username, Password) == true) return true;
+
+            return false;
+
+        }
+    }
+}
